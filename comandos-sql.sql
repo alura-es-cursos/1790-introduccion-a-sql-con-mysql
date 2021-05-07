299b59d47b7cd62c8af831d28d221222d54d4c51
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-05-07 16:04:15 -0300

    aula-3

--- comandos-sql.sql
+++ comandos-sql.sql
@@ -3,3 +3,21 @@
 
 # Ya tu ambiente debe estar listo para trabajar. Te invito ver los videos y a ejecutar todos los 
 # comandos que vas a aprender.
+
+#Los comandos vistos en el aula anterior son los siguientes:
+
+USE world;
+
+SELECT * FROM city;
+
+SELECT * FROM country;
+
+SELECT * FROM countrylanguage;
+
+CREATE DATABASE jugos;
+
+DROP SCHEMA jugos;
+
+SELECT * FROM CITY;
+
+select * from country;
