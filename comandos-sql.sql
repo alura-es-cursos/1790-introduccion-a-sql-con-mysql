436c0bf24aebcdf6b562242e2c2de34976217e9e
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-05-07 16:02:55 -0300

    aula-2

--- comandos-sql.sql
+++ comandos-sql.sql
@@ -1,2 +1,5 @@
 # En esta aula te hablaremos sobre la historia de SQL y de My SQL.
 # También instalaremos Workbench para poder utilizar un IDE en el desarrollo de nuestros ejercicios.
+
+# Ya tu ambiente debe estar listo para trabajar. Te invito ver los videos y a ejecutar todos los 
+# comandos que vas a aprender.
