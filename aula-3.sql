9ff9a74ccf5809c96eed06c02b8bdf2e4722722d
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-05-07 15:37:03 -0300

    Comandos del aula anterior

---
+++ aula-3.sql
@@ -0,0 +1,15 @@
+USE world;
+
+SELECT * FROM city;
+
+SELECT * FROM country;
+
+SELECT * FROM countrylanguage;
+
+CREATE DATABASE jugos;
+
+DROP SCHEMA jugos;
+
+SELECT * FROM CITY;
+
+select * from country;
